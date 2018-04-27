14d19ba75459d379d1bf9a8a7c1514535eab2cc2
SimmaLimma <simonmaartensson@gmail.com>
2018-04-27 11:05:08 +0200

    Update main.m

--- main.m
+++ main.m
@@ -9,8 +9,6 @@ load('testNew.mat')
 % Calculating range [meter] from time [second]. 343 m/s is speed of sound
 rphat = tphat * 343;
 
-
-
 %% Correction tphat, given by Hendeby
 
 %tphat = SFlabFindPulseTimes(recBuffer, recChanList, speakers, sampRate, pulseFreq, pulseWidth, npt, time, plotOn, [], false);
@@ -18,16 +16,12 @@ rphat = tphat * 343;
 %% Estimating measurement noise to estimate R
 meanrphat = mean(rphat')';
 
-%TODO: Is code below redudant?
 e = zeros(size(rphat));
 % e is measurement error for position estimation
 for mic = 1:8
     e(:,mic) = rphat(:,mic) - meanrphat;
 end
 
-
-%TODO: Calculate e with pairwise diff directly, and check if same result
-%Calculating e(i,j) = e(i) - e(j)
 rphatPair = zeros(88,28);
 
 rowInd = 1;
@@ -38,9 +32,7 @@ for i = 1:7
     end
 end
 
-
-%TODO: Try to replace eTmp with tphat(:,mic) - meantphat
-%Calculating e(i,j) = e(i) - e(j)
+% Calculating e(i,j) = e(i) - e(j)
 ePair = zeros(88,28);
 
 rowInd = 1;
@@ -78,7 +70,6 @@ sm2 = exsensor('tdoa2',8,1,2);
 %Start position of robot (at time instance 0)
 startPos = [0.383; 0.095];
 
-%TODO: Make init func
 % x,y value for each sensor in config 1
 micPos1 = [[0, 0.1416*0];
            [0, 0.1416*1];
@@ -108,8 +99,6 @@ sm1.x0 = startPos;
 sm2.x0 = startPos;
 
 % Setting PDF for measurement noise
-% TODO: Try out cov instead of diag(var(ePair)). I think mathematically
-% pretty much same, since mean "takes out eachother"
 sm1.pe = diag(var(ePair));
 sm2.pe = diag(var(ePair));
 
@@ -125,32 +114,29 @@ xlim([0 1.23])
 ylim([0 1])
 %% Analysis of model
 
-% TODO: Code below redudant?
 % Creating SIG object from measurments
 sigrphat = sig(rphat,sampRate);
  
 % CRLB analysis. Uncomment for plot
 % TODO: Can we use another approach than TDOA2? (Since Hendeby said we had to use TDOA1 somewhere)
-%figure(1)
-%hold on
+figure(1)
+hold on
 %Empty y, limits set according to lab setup and calc of RMSE
-%crlb2(sm1, [], 0:0.1:1.3, 0:0.1:1, [1 2], 'rmse'); 
-%title('CRLB analysis of config 1')
+crlb2(sm1, [], 0:0.1:1.3, 0:0.1:1, [1 2], 'rmse'); 
+title('CRLB analysis of config 1')
 
-%figure(2)
-%hold on
+figure(2)
+hold on
 %Empty y, limits set according to lab setup and calc of RMSE
-%crlb2(sm2, [], 0:0.1:1.3, 0:0.1:1, [1 2], 'rmse');  
-%title('CRLB analysis of config 2')
+crlb2(sm2, [], 0:0.1:1.3, 0:0.1:1, [1 2], 'rmse');  
+title('CRLB analysis of config 2')
 
-%hold off
+hold off
 
 
 
 %% Localization
 % Config 2 is choosen as preferred config (due to config analysis)
-
-% TODO: Make this into a function
 
 % Load config 2
 load('testconfig2New.mat')
@@ -159,19 +145,18 @@ load('testconfig2New.mat')
 rphatC2 = 343 * tphat;
 
 % Trajection estimation with pairwise TDOA (all pairs) and config 2
-%estTrajTDOA2 = loc(rphatC2, sm2, 'tdoa2');
+estTrajTDOA2 = loc(rphatC2, sm2, 'tdoa2');
 
 % Plotting trajectory
-%figure(4)
-%plot(estTrajTDOA2(1,:), estTrajTDOA2(2,:))
-%title('Estimated location for object; TDOA2 with all pairs used')
+figure(4)
+plot(estTrajTDOA2(1,:), estTrajTDOA2(2,:))
+title('Localisation; TDOA2 with all pairs used')
 
 % Comparing to ground truth
-%figure(5)
-%SFlabCompEstimGroundTruth(estTrajC2,micPos2)
-%title('Compared estimated location for object; TDOA2 with all pairs used')
+figure(5)
+SFlabCompEstimGroundTruth(estTrajTDOA2,micPos2)
+title('Localisation; TDOA2 with all pairs used')
 
-%% TODO: This loc alg. I have no idea how to use r0 and why it gets so big
 % Trajection estimation with NLS and Guass-Newton search
 sm2nls = exsensor('tdoa1',8,1,2);
 sm2nls.x0 = [startPos' 0.5]';
@@ -181,15 +166,15 @@ sm2nls.pe = diag(var(e));
 estTrajNlsGn = loc(rphatC2, sm2nls, 'nlsGn');
 
 % Plotting trajectory
-%figure(6)
+figure(6)
 
-%plot(estTrajNls3Gn(1,:),estTrajNlsGn(2,:))
-%title('Estimated location for object; NLS with GN search used')
+plot(estTrajNlsGn(1,:),estTrajNlsGn(2,:))
+title('NLS with GN search used')
 
 % Comparing to ground truth
-%figure(7)
-%SFlabCompEstimGroundTruth(estTrajC2,micPos2)
-%title('Compared estimated location for object; NLS with GN search used')
+figure(7)
+SFlabCompEstimGroundTruth(estTrajNlsGn(1:2, :),micPos2)
+title('NLS with GN search used')
 
 %% Choosing motion models
 
@@ -198,12 +183,12 @@ mCv = exnl('cv2d', 0.5);
 
 % Model with Coordinated turn and cartesian velocity
 mCtcv = exnl('ctcv2d', 0.5);
+% TODO: Add cartesian measurements instead of polar
 
 % Artificial measurements from NLS GN Loc. alg.
 artMeasVec = estTrajNlsGn(1:2, :);
 artMeas = sig(estTrajNlsGn(1:2, :)');
 % With Polar coordinates
-
 [thTmp, rTmp] = cart2pol(estTrajNlsGn(1, :), estTrajNlsGn(2, :));
 artMeasPol = sig([rTmp; thTmp]');
 
@@ -236,13 +221,19 @@ xhatCtcvKF = ekf(mCtcv, artMeasPol);
 % Plotting result
 figure(8)
 xplot2(xhatCvKF);
-title('Tracking with CV model and KF')
+title('Tracking with KF and CV model (Artificial measurements)')
 
 figure(9)
-xplot2(xhatCtcvKF)
-title('Tracking with CA model and KF')
+SFlabCompEstimGroundTruth(xhatCvKF.x(:,1:2)',micPos2)
+title(' Tracking with KF and CV model (Artificial measurements)')
 
-%TODO: Plot SFlabCompEstimGroundTruth(estTrajC2,micPos2) somehow
+figure(10)
+xplot2(xhatCtcvKF)
+title('Tracking with EKF and CTCV model (Artificial measurements)')
+
+figure(11)
+SFlabCompEstimGroundTruth(xhatCtcvKF.x(:,1:2)',micPos2)
+title('Tracking with EKF and CTCV model (Artificial measurements)')
 
 %% Estimating using non-linear filter
 
@@ -271,12 +262,12 @@ nn = [4 0 7 16];
 smOneRef = sensormod(h, nn);
 
 % Creating non-linear models with mics added as sensors
-mCvNltmp = exnl('cv2d', 1/2);
+mCvNltmp = exmotion('cv2d', 1/2);
 mCvNl = addsensor(mCvNltmp, smOneRef);
 
 % Tuning the EKF 
 % Tuning Q
-mCvNl.pv = 0.001*eye(4);
+mCvNl.pv = 0.005*eye(4);
 % Tuning R
 mCvNl.pe = diag(var(rphatPairOneRef));
 
@@ -290,5 +281,43 @@ rphatPairSig = sig(rphatPair);
 xhatCvEKF = ekf(mCvNl, rphatPairOneRefSig);
 
 % Plotting result
-figure(10)
+figure(12)
 xplot2(xhatCvEKF)
+title('Tracking with EKF and TOA (one ref) model, using CV model')
+
+figure(13)
+SFlabCompEstimGroundTruth(xhatCvEKF.x(:,1:2)',micPos2)
+title('Tracking with EKF and TOA (one ref) model, using CV model')
+
+
+%%
+% Creating non-linear models with mics added as sensors
+mCtcvNltmp = exmotion('ctcv2d', 1/2);
+mCtcvNl = addsensor(mCtcvNltmp, smOneRef);
+
+% Tuning the EKF 
+% Tuning Q
+mCtcvNl.pv = 0.005*eye(5);
+% Tuning R
+mCtcvNl.pe = diag(var(rphatPairOneRef));
+
+% Init parameters for model CV
+mCtcvNl.px0 = 0.01*eye(5);
+mCtcvNl.x0 = [startPos; 0; 0; 0];
+mCtcvNl.th = micPos2(:);
+
+% Tracking using EKF 
+rphatPairSig = sig(rphatPair);
+xhatCtcvEKF = ekf(mCtcvNl, rphatPairOneRefSig);
+
+% Plotting result
+figure(14)
+xplot2(xhatCtcvEKF)
+title('Tracking with EKF and TOA (one ref) model, using CTCV model')
+
+figure(15)
+SFlabCompEstimGroundTruth(xhatCtcvEKF.x(:,1:2)',micPos2)
+title('Tracking with EKF and TOA (one ref) model, using CTCV model')
+
+
+%% Sensitivity analysis
